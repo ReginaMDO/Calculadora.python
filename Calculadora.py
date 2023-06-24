b7635092b982502d96c7b8e665e9c964f3062022
Regina Oliveira <84687859+ReginaMDO@users.noreply.github.com>
2023-06-24 10:32:15 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,180 +1,151 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMGQ8XDn1PLBG+VeupTAwe0"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "MF8zT53I13ZV"
-      },
-      "outputs": [],
-      "source": [
-        "import math\n",
-        "#Criação da class calculadora\n",
-        "class calculadora:\n",
-        "  def __init_subclass__(self):\n",
-        "    self.num1 = 0\n",
-        "    self.num2 = 0\n",
-        "    self.resultado = 0\n",
-        "\n",
-        "  #Função da adição\n",
-        "  def somar(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 + self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função da subtração\n",
-        "  def subtrair(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 - self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função da multiplicação\n",
-        "  def multiplicar(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 * self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função da divisão\n",
-        "  def dividir(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 / self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função da exponenciação\n",
-        "  def exponenciar(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 ** self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função do resto\n",
-        "  def resto(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = self.num1 % self.num2\n",
-        "    return self.resultado\n",
-        "\n",
-        "  #Função da raiz quadrada\n",
-        "  def raiz(self, num1, num2):\n",
-        "    self.num1 = num1\n",
-        "    self.num2 = num2\n",
-        "    self.resultado = math.sqrt(self.num1 + self.num2)\n",
-        "    return self.resultado\n",
-        "\n",
-        "def continuar(entrada):\n",
-        "  if entrada:\n",
-        "    return True\n",
-        "  else:\n",
-        "    return False\n",
-        "\n",
-        "#Criação do MENU da calculadora\n",
-        "def menu():\n",
-        "  opc = {1: 'Adição',\n",
-        "         2: 'Subtração',\n",
-        "         3: 'Multiplicação',\n",
-        "         4: 'Divisão',\n",
-        "         5: 'Exponenciação',\n",
-        "         6: 'Resto',\n",
-        "         7: 'Raiz'}\n",
-        "  calc = calculadora()\n",
-        "  print('-----------MENU------------')\n",
-        "  print('|--Num---|----Operação----|')\n",
-        "  print('|   1    |     Adição     |')\n",
-        "  print('|   2    |    Subtração   |')\n",
-        "  print('|   3    |  Multiplicação |')\n",
-        "  print('|   4    |     Divisão    |')\n",
-        "  print('|   5    |  Exponenciação |')\n",
-        "  print('|   6    |      Resto     |')\n",
-        "  print('|   7    |      Raiz      |')\n",
-        "  print('---------------------------')\n",
-        "  #Escolha da operação matemática\n",
-        "  print(\"Digite um número para realizar uma operação matemática e aperte ENTER;\")\n",
-        "  calcular = True\n",
-        "  while calcular:\n",
-        "      opcao = input(\"\\nEscolha a opção de cálculo (1, 2, 3, 4, 5, 6 ou 7):\")\n",
-        "      if not(opcao in '1 2 3 4 5 6 7'):\n",
-        "        print(\"Opção inválida!\")\n",
-        "        continue\n",
-        "      else:\n",
-        "        opcao = int(opcao)\n",
-        "        print(f\"Operação matemática escolhida é {opc[opcao]}.\")\n",
-        "\n",
-        "      #Operação da adição\n",
-        "      if opcao == 1:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.somar(num1, num2)\n",
-        "        print(f\"O valor da adição é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação da subtração\n",
-        "      elif opcao == 2:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.subtrair(num1, num2)\n",
-        "        print(f\"O valor da subtração é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação da multiplicação\n",
-        "      elif opcao == 3:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.multiplicar(num1, num2)\n",
-        "        print(f\"O valor da multiplicação é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação da divisão\n",
-        "      elif opcao == 4:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.dividir(num1, num2)\n",
-        "        print(f\"O valor da divisão é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação da exponenciação\n",
-        "      elif opcao == 5:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.exponenciar(num1, num2)\n",
-        "        print(f\"O valor da exponenciação é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação do resto\n",
-        "      elif opcao == 6:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.resto(num1, num2)\n",
-        "        print(f\"O valor do resto é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "\n",
-        "      #Operação da raiz quadrada\n",
-        "      elif opcao == 7:\n",
-        "        num1 = int(input(\"Digite o penúltimo número do seu RU:\"))\n",
-        "        num2 = int(input(\"Digite o último número do seu RU:\"))\n",
-        "        resultado = calc.raiz(num1, num2)\n",
-        "        print(f\"O valor da raiz quadrada da soma dos dois números é {resultado}.\")\n",
-        "        calcular = continuar(input(\"Digite algo para continuar ou aperte ENTER para sair da calculadora: \"))\n",
-        "        print(\"!!!Calculadora finalizada!!!\")"
-      ]
-    }
-  ]
-}
+import math
+#Criação da class calculadora
+class calculadora:
+  def __init_subclass__(self):
+    self.num1 = 0
+    self.num2 = 0
+    self.resultado = 0
+
+  #Função da adição
+  def somar(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 + self.num2
+    return self.resultado
+
+  #Função da subtração
+  def subtrair(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 - self.num2
+    return self.resultado
+
+  #Função da multiplicação
+  def multiplicar(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 * self.num2
+    return self.resultado
+
+  #Função da divisão
+  def dividir(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 / self.num2
+    return self.resultado
+
+  #Função da exponenciação
+  def exponenciar(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 ** self.num2
+    return self.resultado
+
+  #Função do resto
+  def resto(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = self.num1 % self.num2
+    return self.resultado
+
+  #Função da raiz quadrada
+  def raiz(self, num1, num2):
+    self.num1 = num1
+    self.num2 = num2
+    self.resultado = math.sqrt(self.num1 + self.num2)
+    return self.resultado
+
+def continuar(entrada):
+  if entrada:
+    return True
+  else:
+    return False
+
+#Criação do MENU da calculadora
+def menu():
+  opc = {1: 'Adição',
+         2: 'Subtração',
+         3: 'Multiplicação',
+         4: 'Divisão',
+         5: 'Exponenciação',
+         6: 'Resto',
+         7: 'Raiz'}
+  calc = calculadora()
+  print('-----------MENU------------')
+  print('|--Num---|----Operação----|')
+  print('|   1    |     Adição     |')
+  print('|   2    |    Subtração   |')
+  print('|   3    |  Multiplicação |')
+  print('|   4    |     Divisão    |')
+  print('|   5    |  Exponenciação |')
+  print('|   6    |      Resto     |')
+  print('|   7    |      Raiz      |')
+  print('---------------------------')
+  #Escolha da operação matemática
+  print("Digite um número para realizar uma operação matemática e aperte ENTER;")
+  calcular = True
+  while calcular:
+      opcao = input("\nEscolha a opção de cálculo (1, 2, 3, 4, 5, 6 ou 7):")
+      if not(opcao in '1 2 3 4 5 6 7'):
+        print("Opção inválida!")
+        continue
+      else:
+        opcao = int(opcao)
+        print(f"Operação matemática escolhida é {opc[opcao]}.")
+
+      #Operação da adição
+      if opcao == 1:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.somar(num1, num2)
+        print(f"O valor da adição é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação da subtração
+      elif opcao == 2:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.subtrair(num1, num2)
+        print(f"O valor da subtração é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação da multiplicação
+      elif opcao == 3:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.multiplicar(num1, num2)
+        print(f"O valor da multiplicação é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação da divisão
+      elif opcao == 4:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.dividir(num1, num2)
+        print(f"O valor da divisão é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação da exponenciação
+      elif opcao == 5:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.exponenciar(num1, num2)
+        print(f"O valor da exponenciação é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação do resto
+      elif opcao == 6:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.resto(num1, num2)
+        print(f"O valor do resto é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+
+      #Operação da raiz quadrada
+      elif opcao == 7:
+        num1 = int(input("Digite o penúltimo número do seu RU:"))
+        num2 = int(input("Digite o último número do seu RU:"))
+        resultado = calc.raiz(num1, num2)
+        print(f"O valor da raiz quadrada da soma dos dois números é {resultado}.")
+        calcular = continuar(input("Digite algo para continuar ou aperte ENTER para sair da calculadora: "))
+        print("!!!Calculadora finalizada!!!")
